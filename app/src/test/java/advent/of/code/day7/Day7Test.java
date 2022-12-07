2ca31ad1ad77b90cdd274f1b3605874935837665
Brett Yukich <brett.yukich@wise.com>
2022-12-07 09:58:02 +0000

    Set result in test

--- app/src/test/java/advent/of/code/day7/Day7Test.java
+++ app/src/test/java/advent/of/code/day7/Day7Test.java
@@ -5,6 +5,6 @@ import advent.of.code.DailyTest;
 class Day7Test extends DailyTest {
 
   public Day7Test() {
-    super(new Day7(), "95437", "1391690", "24933642", "");
+    super(new Day7(), "95437", "1391690", "24933642", "5469168");
   }
 }
