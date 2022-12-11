0f27fb95525dd138591375815304c5dbf5af7d06
Brett Yukich <brett.yukich@wise.com>
2022-12-11 20:10:29 +0000

    Day 11 - Part 2
    Solved with hints :(

--- app/src/main/java/advent/of/code/day11/Day11.java
+++ app/src/main/java/advent/of/code/day11/Day11.java
@@ -2,6 +2,7 @@ package advent.of.code.day11;
 
 import advent.of.code.DailyChallenge;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -13,45 +14,60 @@ public class Day11 extends DailyChallenge {
 
   @Override
   public String part1(List<String> input) {
+    List<Monkey> monkeys = parseMonkeys(input);
+    Function<BigInteger, BigInteger> relaxFunc = (n) -> n.divide(BigInteger.valueOf(3));
+    return "" + monkeyBusiness(monkeys, 20, relaxFunc);
+  }
+
+  @Override
+  public String part2(List<String> input) {
+    List<Monkey> monkeys = parseMonkeys(input);
+
+    BigInteger relaxFactor = monkeys.stream().map(m -> m.divisor).reduce(BigInteger.ONE, (a, b) -> a.multiply(b));
+    Function<BigInteger, BigInteger> relaxFunc = (n) -> n.mod(relaxFactor);
+
+    return "" + monkeyBusiness(monkeys, 10000, relaxFunc);
+  }
+
+  public List<Monkey> parseMonkeys(List<String> input) {
     List<Monkey> monkeys = new ArrayList<>();
     for (int i = 0; i < input.size(); i += 7) {
       monkeys.add(new Monkey(input.subList(i, i + 6)));
     }
+    return monkeys;
+  }
 
-    for (int round = 0; round < 20; round++ ) {
+  public long monkeyBusiness(List<Monkey> monkeys, int rounds, Function<BigInteger, BigInteger> relaxFunc) {
+    for (int round = 0; round < rounds; round++ ) {
       for (int monkey = 0; monkey < monkeys.size(); monkey++) {
-        monkeys.get(monkey).takeTurns(monkeys);
+        monkeys.get(monkey).takeTurns(monkeys, relaxFunc);
       }
-      System.out.println("After round %s, the monkeys are holding items with these worry levels:".formatted(round));
+//       System.out.println("After round %s, the monkeys are holding items with these worry levels:".formatted(round));
       monkeys.forEach(m -> {
-        System.out.println("Monkey %s: %s".formatted(m.number, m.items.toString()));
+//         System.out.println("Monkey %s: %s".formatted(m.number, m.items.toString()));
       });
     }
 
     return monkeys.stream()
         .map(m -> {
-          System.out.println("Monkey %s inspected items %s times.".formatted(m.number, m.inspections));
+          // System.out.println("Monkey %s inspected items %s times.".formatted(m.number, m.inspections));
           return m.inspections;
         })
         .sorted(Comparator.reverseOrder())
         .limit(2)
-        .reduce(1, (a, b) -> a * b)  // monkey business
-        .toString();
+        .reduce(1L, (a, b) -> a * b);  // monkey business
   }
 
-  @Override
-  public String part2(List<String> input) {
-    return null;
-  }
 
   static class Monkey {
 
-    private int inspections = 0;
+    private long inspections = 0;
 
-    private int number;
-    private List<Integer> items;  // value represents worry level
-    private Function<Integer, Integer> inspectFunc;  //  modifies worry level
-    private Function<Integer, Integer> targetFunc;  // takes a worry level and returns a monkey
+    private long number;
+    private List<BigInteger> items;  // value represents worry level
+    private Function<BigInteger, BigInteger> inspectFunc;  //  modifies worry level
+    private Function<BigInteger, Integer> targetFunc;  // takes a worry level and returns a monkey
+    private BigInteger divisor;
 
     public Monkey(List<String> lines) {
       number = monkeyNumber(lines.get(0));
@@ -60,83 +76,75 @@ public class Day11 extends DailyChallenge {
       targetFunc = buildTargetFunc(lines.subList(3, 6));
     }
 
-    private static int monkeyNumber(String line) {
-      return Integer.parseInt(line.substring(7).replace(":", ""));
+    private static long monkeyNumber(String line) {
+      return Long.parseLong(line.substring(7).replace(":", ""));
     }
 
-    private static List<Integer> items(String line) {
+    private static List<BigInteger> items(String line) {
       return Arrays.stream(line.substring(18).split(","))
           .map(String::trim)
-          .map(Integer::parseInt)
+          .map(BigInteger::new)
           .sorted(Comparator.reverseOrder())
           .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    private static Function<Integer, Integer> buildInspectFunc(String s) {
+    private static Function<BigInteger, BigInteger> buildInspectFunc(String s) {
       String end = s.substring(25);
       if ('*' == s.charAt(23)) {
         if ("old".equals(end)) {
-          return i -> i * i;
+          return i -> i.pow(2);
         } else {
-          return i -> i * Integer.parseInt(end);
+          BigInteger multiplier = new BigInteger(end);
+          return i -> i.multiply(multiplier);
         }
       } else {  // add
         if ("old".equals(end)) {
-          return i -> i + i;
+          BigInteger two = BigInteger.valueOf(2);
+          return i -> i.multiply(two);
         } else {
-          return i -> i + Integer.parseInt(end);
+          BigInteger addition = new BigInteger(end);
+          return i -> i.add(addition);
         }
       }
     }
 
-    private static Function<Integer, Integer> buildTargetFunc(List<String> lines) {
-      int divisor = Integer.parseInt(lines.get(0).substring(21));
+    private Function<BigInteger, Integer> buildTargetFunc(List<String> lines) {
+      divisor = new BigInteger(lines.get(0).substring(21));
       int monkeyA = Integer.parseInt(lines.get(1).trim().split(" ")[5]);  // true
       int monkeyB = Integer.parseInt(lines.get(2).trim().split(" ")[5]);  // false
       return i -> {
-        if (i % divisor == 0) {
-          System.out.println("    Current worry level is divisible by %s.".formatted(divisor));
+        if (i.mod(divisor).equals(BigInteger.ZERO)) {
+          // System.out.println("    Current worry level is divisible by %s.".formatted(divisor));
           return monkeyA;
         } else {
-          System.out.println("    Current worry level is not divisible by %s.".formatted(divisor));
+          // System.out.println("    Current worry level is not divisible by %s.".formatted(divisor));
           return monkeyB;
         }
       };
     }
 
-    public int target(Integer item) {
+    int target(BigInteger item) {
       return targetFunc.apply(item);
     }
 
-    public void catchItem(Integer item) {
+    void catchItem(BigInteger item) {
       items.add(0, item);
     }
 
-    void inspect() {
-      inspections++;
-      int currentWorryLevel = items.remove(items.size() - 1);
-      System.out.println("  Monkey inspects an item with a worry level of " + currentWorryLevel);
-      int newWorryLevel = inspectFunc.apply(currentWorryLevel);
-      System.out.println("    Worry level is adjusted to " + newWorryLevel);
-      items.add(newWorryLevel);
-    }
-
-    public void relax() {
-      int currentWorryLevel = items.remove(items.size() - 1);
-      int newWorryLevel = currentWorryLevel / 3;  // divide by three and round down
-      System.out.println("    Monkey gets bored with item. Worry level is divided by 3 to " + newWorryLevel);
-      items.add(newWorryLevel);
-    }
-
-    public void takeTurns(List<Monkey> monkeys) {
-      System.out.println("Monkey " + number);
+    void takeTurns(List<Monkey> monkeys, Function<BigInteger, BigInteger> relaxFunc) {
+      // System.out.println("Monkey " + number);
       while (!items.isEmpty()) {
-        inspect();
-        relax();
-        int item = items.remove(items.size() - 1);  // remove last item
-        int target = target(item);
-        System.out.println("    Item with worry level %s is thrown to monkey %s".formatted(item, target));
-        monkeys.get(target).catchItem(item);
+        inspections++;
+        BigInteger worry = items.remove(items.size() - 1);
+        // System.out.println("  Monkey inspects an item with a worry level of " + currentWorryLevel);
+        worry = inspectFunc.apply(worry);
+        // System.out.println("    Worry level is adjusted to " + newWorryLevel);
+
+        worry = relaxFunc.apply(worry);
+
+        int target = target(worry);
+        // System.out.println("    Item with worry level %s is thrown to monkey %s".formatted(item, target));
+        monkeys.get(target).catchItem(worry);
       }
     }
   }
--- app/src/test/java/advent/of/code/day11/Day11Test.java
+++ app/src/test/java/advent/of/code/day11/Day11Test.java
@@ -5,6 +5,6 @@ import advent.of.code.DailyTest;
 class Day11Test extends DailyTest {
 
   public Day11Test() {
-    super(new Day11(), "10605", "117624", "", "");
+    super(new Day11(), "10605", "117624", "2713310158", "16792940265");
   }
 }
