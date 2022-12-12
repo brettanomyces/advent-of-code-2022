83d418df38d626a7c22d2d9e40240f568487f5e7
Brett Yukich <brett.yukich@wise.com>
2022-12-12 08:10:41 +0000

    Day 12 - setup

---
+++ app/src/main/java/advent/of/code/day12/Day12.java
@@ -0,0 +1,25 @@
+package advent.of.code.day12;
+
+import advent.of.code.DailyChallenge;
+
+import java.util.List;
+
+class Day12 extends DailyChallenge {
+
+  // a = 97 ... z = 122
+  // S = start / current position, starts at 'a'
+  // E = end, == 'z'
+  // can step to at most one higher, but as many lower as we'd like
+
+  @Override
+  public String part1(List<String> input) {
+    return null;
+  }
+
+  @Override
+  public String part2(List<String> input) {
+    return null;
+  }
+
+
+}
---
+++ app/src/test/java/advent/of/code/day12/Day12Test.java
@@ -0,0 +1,13 @@
+package advent.of.code.day12;
+
+import advent.of.code.DailyChallenge;
+import advent.of.code.DailyTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day12Test extends DailyTest {
+
+  public Day12Test() {
+    super(new Day12(), null, null, null, null);
+  }
+}
